90e903864c9984832345d2a4d9511f1a801a4874
Mohamed Imad <email2emad@gmail.com>
2020-12-28 14:58:47 +0530

    Added Middleware to payslips

--- app/Http/Controllers/PayslipController.php
+++ app/Http/Controllers/PayslipController.php
@@ -9,6 +9,14 @@ use Illuminate\Http\Request;
 class PayslipController extends Controller
 {
     /**
+     * Create a new instance with auth as middleware
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
